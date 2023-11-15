aaa60d87babb4d63d75a36ac5a2c107eb3c38ebb
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2023-11-15 23:17:06 +0300

    Methods recreated for sqlite with context

---
+++ storage/sqlite/sqlite.go
@@ -0,0 +1,92 @@
+package sqlite
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/Jhnvlglmlbrt/tg-bot/lib/e"
+	"github.com/Jhnvlglmlbrt/tg-bot/storage"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+// New creates new SQLite storage.
+func New(path string) (*Storage, error) {
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		return nil, e.Wrap("can't open database", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, e.Wrap("can't connect to database", err)
+	}
+
+	return &Storage{db: db}, nil
+}
+
+// Save saves page to storage.
+func (s *Storage) Save(ctx context.Context, p *storage.Page) error {
+	q := `INSERT INTO pages (url, user_name) VALUES (?, ?)`
+
+	if _, err := s.db.ExecContext(ctx, q, p.URL, p.UserName); err != nil {
+		return e.Wrap("can't save page", err)
+	}
+
+	return nil
+}
+
+// PickRandom picks random page from storage.
+func (s *Storage) PickRandom(ctx context.Context, userName string) (*storage.Page, error) {
+	q := `SELECT url FROM pages WHERE user_name = ? ORDER BY RANDOM() LIMIT 1`
+
+	var url string
+
+	err := s.db.QueryRowContext(ctx, q, userName).Scan(&url)
+	if err == sql.ErrNoRows {
+		return nil, storage.ErrNoSavedPages
+	}
+	if err != nil {
+		return nil, e.Wrap("can't open database", err)
+	}
+
+	return &storage.Page{
+		URL:      url,
+		UserName: userName,
+	}, nil
+}
+
+// Remove removes page from storage.
+func (s *Storage) Remove(ctx context.Context, page *storage.Page) error {
+	q := `DELETE FROM pages WHERE url = ? AND user_name = ?`
+
+	if _, err := s.db.ExecContext(ctx, q, page.URL, page.UserName); err != nil {
+		return e.Wrap("can't remove page", err)
+	}
+
+	return nil
+}
+
+// IsExists checks if page exists in storage.
+func (s *Storage) IsExists(ctx context.Context, page *storage.Page) (bool, error) {
+	q := `SELECT COUNT(*) FROM pages WHERE url = ? AND user_name = ?`
+
+	var count int
+
+	if err := s.db.QueryRowContext(ctx, q, page.URL, page.UserName).Scan(&count); err != nil {
+		return false, e.Wrap("can't check if page exists", err)
+	}
+
+	return count > 0, nil
+}
+
+func (s *Storage) Init(ctx context.Context) error {
+	q := `CREATE TABLE IF NOT EXISTS pages (url TEXT, user_name TEXT)`
+
+	if _, err := s.db.ExecContext(ctx, q); err != nil {
+		return e.Wrap("can't create table", err)
+	}
+
+	return nil
+}
