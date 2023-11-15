69b4599bd25130ef0291a9ada50c5edf1c6ced24
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2023-11-15 23:18:13 +0300

    added sqlite init

--- main.go
+++ main.go
@@ -1,25 +1,37 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 
 	tgClient "github.com/Jhnvlglmlbrt/tg-bot/clients/telegram"
 	event_consumer "github.com/Jhnvlglmlbrt/tg-bot/consumer/event-consumer"
 	"github.com/Jhnvlglmlbrt/tg-bot/events/telegram"
-	"github.com/Jhnvlglmlbrt/tg-bot/storage/files"
+	"github.com/Jhnvlglmlbrt/tg-bot/storage/sqlite"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const (
-	storagePath = "files_storage"
-	batchSize   = 100
+	storagePath       = "files_storage"
+	sqliteStoragePath = "data/sqlite/storage.db"
+	batchSize         = 100
 )
 
 func main() {
+	// s := files.New(storagePath)
+	s, err := sqlite.New(sqliteStoragePath)
+	if err != nil {
+		log.Fatal("can't connect to storage: ", err)
+	}
+
+	if err = s.Init(context.TODO()); err != nil {
+		log.Fatal("can't init storage: ", err)
+	}
 
 	eventsProcessor := telegram.New(
 		tgClient.New(mustTokenHost()),
-		files.New(storagePath),
+		s,
 	)
 
 	log.Print("Service started")
