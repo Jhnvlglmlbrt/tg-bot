9e6813df68b02d9a0030a442e9003d7a38b64a3a
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2023-11-16 21:33:03 +0300

    added list method to structure

--- storage/storage.go
+++ storage/storage.go
@@ -15,6 +15,7 @@ type Storage interface {
 	PickRandom(ctx context.Context, userName string) (*Page, error)
 	Remove(ctx context.Context, p *Page) error
 	IsExists(ctx context.Context, p *Page) (bool, error)
+	List(ctx context.Context) (string, error)
 }
 
 var ErrNoSavedPages = errors.New("no saved pages")
